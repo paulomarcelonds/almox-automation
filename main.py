0b92e63b7ca1ea58961876652b85734290ef561b
paulomarcelonds <paulomarcelonds@gmail.com>
2024-09-30 14:25:30 -0300

    carregando e inserindo arquivos banco de dados

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import pandas as pd
 import numpy as np
+from pathlib import Path
 
-caminho_arquivo = r'H:\Meu Drive\relatorio-analitico\r.xlsx'
 
-df = pd.read_excel(caminho_arquivo)
-print(df)
+
+# esturura repetição pasta imperium
+drive_imperium = Path(r'i:\Meu Drive\automation-digitec\fortaleza\atendimento')
+for file in drive_imperium.glob("*.csv"):
+    print(file.name)
+
+
+
+# Função para carregar arquivos da pasta
+def carregar_arquivos(pasta):
+    arquivos = list(pasta.glob("*.csv"))  # Buscar todos os arquivos .csv
+    for arquivo in arquivos:
+        print(f"Carregando {arquivo.name}")
+        # Aqui você pode colocar o que fazer com o arquivo, por exemplo, abrir com pandas
+        # df = pd.read_csv(arquivo)
+        # print(df.head())  # Exemplo de visualização dos dados
+    if not arquivos:
+        print("Nenhum arquivo encontrado.")
+
+# Chamando a função para carregar os arquivos
+carregar_arquivos(drive_imperium)
+
